94b5f43f057e759b49ec53d55cff992d83b56f00
ElxanQuliyev <47784245+ElxanQuliyev@users.noreply.github.com>
2019-12-13 22:52:38 +0400

    Add files via upload

---
+++ 12.11.2019/12.11.2019/MyClasses/CoxBucaqli.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._11._2019.MyClasses
+{
+    class CoxBucaqli
+    {
+        protected int teref;
+        
+        public CoxBucaqli(int tr)
+        {
+            teref = tr;
+        }
+        public virtual int Sahe()
+        {
+            return teref;
+        }
+    }
+    class DordBucaqli : CoxBucaqli
+    {
+        public DordBucaqli(int dordteref):base(dordteref)
+        {}
+        public override int Sahe()
+        {
+            return teref * 4;
+        }
+    }
+    class Ucbucaq:CoxBucaqli
+    {
+        public Ucbucaq(int ucteref):base(ucteref){}
+        
+        public override int  Sahe()
+        {
+            return teref*3;
+        }
+    }
+
+
+}
---
+++ 12.11.2019/12.11.2019/Program.cs
@@ -0,0 +1,23 @@
+﻿using _12._11._2019.MyClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._11._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zehmet olmasa teref uzunlugunu qeyd edin:");
+            int trf = Convert.ToInt32(Console.ReadLine());
+            DordBucaqli drd = new DordBucaqli(trf);
+            Ucbucaq ucb = new Ucbucaq(trf);
+            Console.WriteLine("Dordbucaqlinin sahesi:{0}",drd.Sahe()+"sm");
+            Console.WriteLine("Ucbucaqlinin sahesi:{0}",ucb.Sahe()+"sm");
+        }
+    }
+}
+
